0499d663294bcfecebc39f01419a5f83bba07f4f
argilmutdinov <alergilm@gmail.com>
2024-11-14 21:24:43 +0300

    added exceptions in triangle.py

--- triangle.py
+++ triangle.py
@@ -6,10 +6,9 @@ def area(a, b, c):
     Принимает три стороны треугольника a b c.
     Возвращает площадь треугольника
     """
-    if (a < b + c and b < a + c and c < a + b):
-        p = (a + b + c) / 2
+    try:
         return math.sqrt(p * (p - a) * (p - b) * (p - c))
-    else:
+    except:
         return "Triangle doesn't exist"
 
 
